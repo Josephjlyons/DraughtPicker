7863fb8bd070b054b65dd4d9506fdbb249810f62
Heath Bennett <heathben418@gmail.com>
2021-01-30 11:01:09 -0500

    Mapquest API

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
-const mapQuest
-
 $.ajax({
     url: queryURLOne,
     method: "GET"
 }).then(function(response) {
-
-
-console.log(response);
     
     
+    console.log(response);
+    
+    const lat = response.latitude;
+    const long = response.longitude;
+    const mapQKey = "tm9ssbyvHrxMSsgIhCIymXmOzGvEGYZr"
+    const mapQuest = "https://www.mapquestapi.com/staticmap/v5/map?key=" + mapQKey + "&center=" + lat + "," + long;
     
     $.ajax({
     url: mapQuest,
