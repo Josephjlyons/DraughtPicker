acde1e5645ddfadb8903cce6f3062d71057cd4eb
Heath Bennett <heathben418@gmail.com>
2021-01-30 10:28:45 -0500

    empty ajax request

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+
+const mapQuest
+
+$.ajax({
+    url: queryURLOne,
+    method: "GET"
+}).then(function(response) {
+
+
+console.log(response);
+    
+    
+    
+    $.ajax({
+    url: mapQuest,
+    method: "GET"
+    }).then(function(responseTwo) {
+
+    console.log(responseTwo);
+
+    
+    });
+});
