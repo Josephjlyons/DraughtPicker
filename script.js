8d240e760c30230175ca59823ef8b67d4e311d0e
Tristan LaRoche <laroche11235@gmail.com>
2021-02-01 20:35:13 -0500

    moved mapQuest inside loop and alt img.map

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     // Variable For User Input 
 
     let searchBtn = $("#searchBtn");
+    const mapQKey = "tm9ssbyvHrxMSsgIhCIymXmOzGvEGYZr"
 
     searchBtn.click(function () {
 
@@ -37,55 +38,62 @@ $(document).ready(function () {
         }).then(function (responseOne) {
             // console.log(responseOne)
 
-            let name = responseOne.name;
-            let brewType = responseOne.brewery_type;
-            let zip = responseOne.postal_code;
-            let city = responseOne.city;
-            let state = responseOne[0].state;
-            let phone = responseOne.phone;
-            let website = responseOne.website_url;
-            const lat = responseOne[0].latitude;
-            const long = responseOne[0].longitude;
-            const street = responseOne[0].street;
+            // let name = responseOne.name;
+            // let brewType = responseOne.brewery_type;
+            // let zip = responseOne.postal_code;
+            // let city = responseOne.city;
+            // let state = responseOne[0].state;
+            // let phone = responseOne.phone;
+            // let website = responseOne.website_url;
+            // const lat = responseOne[0].latitude;
+            // const long = responseOne[0].longitude;
+            // const street = responseOne[0].street;
+            // const mapQuest = `https://www.mapquestapi.com/staticmap/v5/map?locations=${lat},${long},${state}&key=${mapQKey}&zoom=16&banner=${streetNumb}+${streetName}`;
+            // $("img.map").attr("src", mapQuest);
 
 
 
-            sepAddress(street);
+            // sepAddress(street);
 
 
             function populateCarousel(data) {
-                let carousel = $(".carousel")
                 // carousel.empty();
 
                 let $carouselInnerEl = $(".carousel-inner")
                 let $carouselindicatorsEL = $(".carousel-indicators")
                 //   console.log(data);
                 for (let i = 0; i < data.length; i++) {
-                        let ilEL = $(`<li data-target="#myCarousel" data-slide-to="${i}" class="${i === 0? "active": ""}"></li>`)
-                        let divItemEL = $(`<div class="item ${i === 0? "active": ""}"></div`)
-                        let brewDataEL = $(`
+                    let state = responseOne[i].state;
+                    const lat = responseOne[i].latitude;
+                    const long = responseOne[i].longitude;
+                    const street = responseOne[i].street;
+                    sepAddress(street);
+                    const mapQuest = `https://www.mapquestapi.com/staticmap/v5/map?locations=${lat},${long},${state}&key=${mapQKey}&zoom=16&banner=${streetNumb}+${streetName}`;
+                    $("img.map").attr("src", mapQuest);
+                    let ilEL = $(`<li data-target="#myCarousel" data-slide-to="${i}" class="${i === 0 ? "active" : ""}"></li>`)
+                    let divItemEL = $(`<div class="item ${i === 0 ? "active" : ""}"></div`)
+                    let brewDataEL = $(`
                             <p>Name: ${data[i].name}</p><br><br>
                             <p>Brewery Type: ${data[i].brewery_type}</p><br><br>
                             <p>City: ${data[i].city}</p><br><br>
                             <p>Street Address: ${data[i].street}</p><br><br>
                             <p>Phone: ${data[i].phone}</p><br><br>
                             <p>Website: <a href="${data[i].website_url}"> ${data[i].website_url}</a></p><br><br>
-                            <img class="map" src="//:0" alt="">
+                            <img class="map" src="//:0" alt="This is a map of ${data[i].name} location">
                         `)
-                        divItemEL.append(brewDataEL)
-                        $carouselindicatorsEL.append(ilEL)
-                        $carouselInnerEl.append(divItemEL)
+                    divItemEL.append(brewDataEL)
+                    $carouselindicatorsEL.append(ilEL)
+                    $carouselInnerEl.append(divItemEL)
                 }
             }
 
             populateCarousel(responseOne);
 
 
-            const mapQKey = "tm9ssbyvHrxMSsgIhCIymXmOzGvEGYZr"
 
 
-            const mapQuest = `https://www.mapquestapi.com/staticmap/v5/map?locations=${lat},${long},${state}&key=${mapQKey}&zoom=16&banner=${streetNumb}+${streetName}`;
-            $("img.map").attr("src", mapQuest);
+
+
             console.log(mapQuest);
 
             // $.ajax({
