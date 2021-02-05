023bc5ca9ad59e56ba5c49671b58ec4b2b31fb95
Tristan LaRoche <laroche11235@gmail.com>
2021-02-05 18:38:00 -0500

    in favorite website is now a link

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
                 <p>${favoriteArr[i].city}</p><br>
                 <p>${favoriteArr[i].StreetAdd}</p><br>
                 <p>${favoriteArr[i].phone}</p><br>
-                <p>${favoriteArr[i].website}</p>
+                <p><a href="${favoriteArr[i].website}">${favoriteArr[i].website}</a></p>
             </div>
         </li>`)
         favoritesListEl.append(favoriteListLiEL)
@@ -73,38 +73,40 @@ $(document).ready(function () {
                     sepAddress(street);
                     const mapQuest = `https://www.mapquestapi.com/staticmap/v5/map?locations=${lat},${long},${state}&key=${mapQKey}&zoom=16&size=400,350&defaultMarker=marker-26A69A&banner=${streetNumb}+${streetName}|26A69A`;
                     let divItemEL = $(`<div class="item ${i === 0 ? "active" : ""}"></div`)
-                    let city = data[i].city;
-                    let streetAdd = data[i].street;
-                    let phone = data[i].phone;
-                    let webSite = data[i].website_url;
-                    let type = data[i].brewery_type;
+                    
+                    
+                    // let city = data[i].city;
+                    // let streetAdd = data[i].street;
+                    // let phone = data[i].phone;
+                    // let webSite = data[i].website_url;
+                    // let type = data[i].brewery_type;
 
-                    if (city === "" || city === "null") {
-                        city = "Information was not provided.";
-                    }
-                    if (streetAdd === "" || streetAdd === "null") {
-                        streetAdd = "Information was not provided.";
-                    }
-                    if (phone === "" || phone === "null") {
-                        phone = "Information was not provided.";
-                    }
-                    else {
-                        phone = [phone.slice(0, 3) + "-" + phone.slice(3, 6) + "-" + phone.slice(6)].join('');
-                    }
-                    if (webSite === "" || webSite === null) {
-                        webSite = "Information was not provided.";
-                    }
+                    // if (city === "" || city === "null") {
+                    //     city = "Information was not provided.";
+                    // }
+                    // if (streetAdd === "" || streetAdd === "null") {
+                    //     streetAdd = "Information was not provided.";
+                    // }
+                    // if (phone === "" || phone === "null") {
+                    //     phone = "Information was not provided.";
+                    // }
+                    // else {
+                    //     phone = [phone.slice(0, 3) + "-" + phone.slice(3, 6) + "-" + phone.slice(6)].join('');
+                    // }
+                    // if (webSite === "" || webSite === null) {
+                    //     webSite = "Information was not provided.";
+                    // }
 
-                    type = type.charAt(0).toUpperCase() + type.slice(1);
+                    // type = type.charAt(0).toUpperCase() + type.slice(1);
 
                     let brewDataEL = $(`
                             <div class="brewData">
                                 <p>Name: <i class="name">${data[i].name}</i></p><br>
-                                <p class="brewType">Brewery Type: ${type} </p><br>
-                                <p class="city">City: ${city}</p><br>
-                                <p class="StreetAdd">Street Address: ${streetAdd}</p><br>
-                                <p class="phone">Phone: ${phone}</p><br>
-                                <p class="website">Website: <a href="${data[i].website_url}"> ${webSite}</a></p><br>
+                                <p class="brewType">Brewery Type: ${data[i].brewery_type.charAt(0).toUpperCase() + data[i].brewery_type.slice(1)} </p><br>
+                                <p class="city">City: ${data[i].city === "" ? "Information was not provided." : data[i].city}</p><br>
+                                <p class="StreetAdd">Street Address: ${data[i].street === "" ? "Information was not provided." : data[i].street}</p><br>
+                                <p class="phone">Phone: ${data[i].phone === "" ? "Information was not provided." : data[i].phone}</p><br>
+                                <p class="website">Website: ${data[i].website_url === "" ? "Information was not provided." : "<a href=" + data[i].website_url + ">" + data[i].website_url + "</a>"}</p><br>
                                 <button class="favoriteBtn">Save To Favorites</button><br>                            
                             </div>
                             <img class="map" src="${lat === null ? "./assets/mapNotAvailable.png" : mapQuest}" alt="This is a map of ${data[i].name} location"><br><br><br>
@@ -150,7 +152,7 @@ $(document).ready(function () {
                             <p>${favoriteArr[i].city}</p><br>
                             <p>${favoriteArr[i].StreetAdd}</p><br>
                             <p>${favoriteArr[i].phone}</p><br>
-                            <p>${favoriteArr[i].website}</p>
+                            <p><a href="${favoriteArr[i].website}">${favoriteArr[i].website}</a></p>
                         </div>
                     </li>`)
                     favoritesListEl.append(favoriteListLiEL)
